004a928017b6f2c080f935053f34a7e235875c3c
tocanaclaudiu <148591148+tocanaclaudiu@users.noreply.github.com>
2024-06-01 18:07:03 +0300

    Add files via upload

---
+++ BigBank/BigBank/BigBank/BigBank.cpp
@@ -0,0 +1,59 @@
+
+#include "pch.h"
+#include "CurrentAccount.h"
+#include "SavingsAccount.h"
+
+using namespace System;
+
+
+
+int main(array<System::String^>^ args)
+{
+    Console::WriteLine("Testing the CurrentAccount");
+    CurrentAccount current("Jane", 100);
+    current.Credit(500);
+
+    if (current.Debit(550) == true) {
+        Console::WriteLine("Debit(550) OK");
+    }
+    else {
+        Console::WriteLine("Debit(550) FAILED");
+    }
+
+    if (current.Debit(100) == true) {
+        Console::WriteLine("Debit(100) OK");
+    }
+    else
+    {
+        Console::WriteLine("Debit(100) FAILED");
+    }
+
+    Console::WriteLine(current.ToString());
+    Console::WriteLine();
+
+    Console::WriteLine("Testing the SavingsAccount");
+    SavingsAccount savings("Fred");
+    savings.Credit(500);
+
+    if (savings.Debit(50) == true) {
+        Console::WriteLine("Debit(50) OK");
+    }
+    else {
+        Console::WriteLine("Debit(50) FAILED");
+    }
+
+    if (savings.Debit(46) == true) {
+        Console::WriteLine("Debit(46) OK");
+    }
+    else
+    {
+        Console::WriteLine("Debit(46) FAILED");
+    }
+
+    Console::WriteLine(savings.ToString());
+
+
+
+
+    return 0;
+}
